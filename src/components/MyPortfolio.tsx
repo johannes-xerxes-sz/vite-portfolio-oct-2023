e6245cd4969809bb1f1204a6f0f2180a040e9a42
johannes-xerxes-sz <johannes.dalogdog@supportzebra.com>
2023-10-24 10:56:19 +0800

    need carousel and currently responsiveness

--- src/components/MyPortfolio.tsx
+++ src/components/MyPortfolio.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import { SectionTitle, SectionDescription } from "./style/Landing.styled";
 
@@ -15,71 +15,29 @@ const DividerTitle = styled.hr`
   margin-bottom: 50px;
 `;
 
-const ImageCarousel = styled.div`
-  width: 100%;
-  max-width: 600px;
-  margin: 0 auto;
-  overflow: hidden;
-  position: relative;
-`;
-
-const CarouselSlide = styled.div`
-  width: 100%;
+const ImageContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  transition: transform 0.5s;
+  overflow-x: auto; /* Add horizontal scrolling if needed */
 `;
 
 const CarouselImage = styled.img`
-  width: 100%;
-  height: auto;
-`;
-
-const PreviousButton = styled.button`
-  position: absolute;
-  top: 50%;
-  left: 0;
-  transform: translateY(-50%);
-  background: transparent;
-  border: none;
-  cursor: pointer;
-  font-size: 24px;
-`;
-
-const NextButton = styled.button`
-  position: absolute;
-  top: 50%;
-  right: 0;
-  transform: translateY(-50%);
-  background: transparent;
-  border: none;
-  cursor: pointer;
-  font-size: 24px;
+  width: auto;
+  max-height: 200px; /* Adjust the max height as needed */
+  margin-right: 20px; /* Add spacing between images */
 `;
 
 const images = [
   "https://websitedemos.net/personal-portfolio-02/wp-content/uploads/sites/770/2021/02/portfolio-3.png",
   "https://websitedemos.net/personal-portfolio-02/wp-content/uploads/sites/770/2021/02/portfolio-2.png",
-  "https://websitedemos.net/personal-portfolio-02/wp-content/uploads/sites/770/2021/02/portfolio-1.png",
+  "https://websitedemos.net/personal-portfolio-02/wp-content/uploads/sites/770/2021/02/portfolio-6-1.png",
+  "https://websitedemos.net/personal-portfolio-02/wp-content/uploads/sites/770/2021/02/portfolio-4.png",
+  "https://websitedemos.net/personal-portfolio-02/wp-content/uploads/sites/770/2021/02/portfolio-7-1.png",
   // Add more image URLs here for your carousel
 ];
 
 const Portfolio: React.FC = () => {
-  const [currentImage, setCurrentImage] = useState(0);
-
-  const goToPreviousImage = () => {
-    if (currentImage > 0) {
-      setCurrentImage(currentImage - 1);
-    }
-  };
-
-  const goToNextImage = () => {
-    if (currentImage < images.length - 1) {
-      setCurrentImage(currentImage + 1);
-    }
-  };
-
   return (
     <PortfolioContainer>
       <SectionTitle>My Portfolio</SectionTitle>
@@ -87,19 +45,15 @@ const Portfolio: React.FC = () => {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
       </SectionDescription>
       <DividerTitle />
-      <ImageCarousel>
-        <CarouselSlide
-          style={{
-            transform: `translateX(-${currentImage * 100}%)`,
-          }}
-        >
-          {images.map((image, index) => (
-            <CarouselImage key={index} src={image} alt={`Slide ${index + 1}`} />
-          ))}
-        </CarouselSlide>
-        <PreviousButton onClick={goToPreviousImage}>{"<"}</PreviousButton>
-        <NextButton onClick={goToNextImage}>{">"}</NextButton>
-      </ImageCarousel>
+      <ImageContainer>
+        {images.map((image, index) => (
+          <CarouselImage
+            key={index}
+            src={image}
+            alt={`Portfolio Image ${index}`}
+          />
+        ))}
+      </ImageContainer>
     </PortfolioContainer>
   );
 };
